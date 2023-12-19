81fb45850d6c8c3b10051cb644198e4c1a991e26
Carlos <carloscruzp06@gmail.com>
2023-12-19 21:36:41 +0100

    [Day 6] Part 2 solution

--- src/main/kotlin/day6/Race.kt
+++ src/main/kotlin/day6/Race.kt
@@ -8,16 +8,16 @@ import tools.StringHandler
  * @property duration Duration of this Race.
  * @property record Record distance achieved in this Race.
  */
-class Race (val duration: Int, val record: Int) {
+class Race (val duration: Long, val record: Long) {
 
     /**
      * Get the distance covered in this Race by pressing the boat's button for [chargingTime] milliseconds.
      *
-     * @param chargingTime [Int] amount of milliseconds to press the boat's button.
-     * @return [Int] that represents the distance covered by the boat when the time is over.
+     * @param chargingTime [Long] amount of milliseconds to press the boat's button.
+     * @return [Long] that represents the distance covered by the boat when the time is over.
      * @throws IllegalArgumentException if the given [chargingTime] is negative.
      */
-    fun getDistance(chargingTime: Int): Int {
+    fun getDistance(chargingTime: Long): Long {
         when {
             chargingTime < 0 -> throw IllegalArgumentException("Time cannot be negative!")
             chargingTime > duration -> return 0
@@ -34,38 +34,27 @@ class Race (val duration: Int, val record: Int) {
      * @param distance Distance covered by the boat in this Race.
      * @return _true_ if the boat breaks the current record; otherwise returns _false_.
      */
-    fun breaksRecord(distance: Int): Boolean {
+    fun breaksRecord(distance: Long): Boolean {
         return record < distance
     }
 }
 
 fun main() {
     val input = StringHandler.readFile(filename = "src/main/resources/boatRaces.txt").lines()
-    val times = input[0].split(':')[1].trim().split(' ').filter { it.isNotBlank() }
-    val records = input[1].split(':')[1].trim().split(' ').filter { it.isNotBlank() }
+    val times = input[0].split(':')[1].filterNot { it.isWhitespace() }
+    val records = input[1].split(':')[1].filterNot { it.isWhitespace() }
 
-    if (times.size != records.size) {
-        throw IllegalArgumentException("Bad input file!")
-    }
-
-    val races = mutableListOf<Race>()
-    for (i in times.indices) {
-        races.add(Race(duration = times[i].toInt(), record = records[i].toInt()))
-    }
+    val races = mutableListOf(Race(duration = times.toLong(), record = records.toLong()))
 
     val winning = races.map {
         var possibleWin = 0
-        println("=====Race=====")
         for (i in 0..it.duration) {
             val distance = it.getDistance(chargingTime = i)
-            println("Charge = $i -> distance = $distance breaks record(${it.record})? -> ${it.breaksRecord(distance)}")
             possibleWin += if (it.breaksRecord(distance = distance)) { 1 } else 0
         }
-        println("Possible wins: $possibleWin\n")
         possibleWin
     }
 
-    println("Error margins -> $winning")
     val totalMargin = winning.foldRight(initial = 1) { margin: Int, acc: Int -> margin * acc }
     println("Total margin -> $totalMargin")
 }
